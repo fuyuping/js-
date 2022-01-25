aa974f3bc9835ebc28f0162ec08145fe2c54a356
fuyuping <34242606+fuyuping@users.noreply.github.com>
2022-01-25 14:25:52 +0800

    Update fill.js

--- fill.js
+++ fill.js
@@ -1,9 +1,9 @@
-Array.prototype.myFill = function(value, start=0, end=this.length) {
-    const len = this.length
-    start = start < 0 ? start + len:start
-    end = end < 0 ? end + len:end
-    for(let i = start; i < end; i++) {
-        this[i] = value
-    }
-    return this;
-}
+Array.prototype.myFill = function (value, start = 0, end = this.length) {
+  const len = this.length;
+  start = start < 0 ? start + len : start;
+  end = end < 0 ? end + len : end;
+  for (let i = start; i < end; i++) {
+    this[i] = value;
+  }
+  return this;
+};
