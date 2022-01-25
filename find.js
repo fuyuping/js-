e8da6925bb73d2b93f58af42c7f30cc5625a3a21
fuyuping <34242606+fuyuping@users.noreply.github.com>
2022-01-25 14:26:59 +0800

    Update find.js

--- find.js
+++ find.js
@@ -1,8 +1,8 @@
-Array.prototype.myFind = function(cb, thisArg) {
-    cb = cb.bind(thisArg)
-    for(let i = 0; i < this.length; i++) {
-        if(cb(this[i])) {
-            return this[i]
-        }
+Array.prototype.myFind = function (cb, thisArg) {
+  cb = cb.bind(thisArg);
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i])) {
+      return this[i];
     }
-}
+  }
+};
