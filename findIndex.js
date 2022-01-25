f2ad26c2980c02125e13ad3e5b0b9f392dcc783e
fuyuping <34242606+fuyuping@users.noreply.github.com>
2022-01-25 14:27:32 +0800

    Update findIndex.js

--- findIndex.js
+++ findIndex.js
@@ -1,9 +1,9 @@
-Array.prototype.myFindIndex = function(cb, thisArg) {
-    cb = cb.bind(thisArg)
-    for(let i = 0; i < this.length; i++) {
-        if(cb(this[i])) {
-            return i
-        }
+Array.prototype.myFindIndex = function (cb, thisArg) {
+  cb = cb.bind(thisArg);
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i])) {
+      return i;
     }
-    return -1
-}
+  }
+  return -1;
+};
