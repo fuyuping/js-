d62ebe65c22807f199dd28e4f8f954e4552c7dc7
fuyuping <34242606+fuyuping@users.noreply.github.com>
2022-01-25 14:29:28 +0800

    Update includes.js

--- includes.js
+++ includes.js
@@ -1,18 +1,18 @@
 Array.prototype.myIncludes = function (param, start = 0) {
-    const len = this.length
-    if (start >= this.length) {
-        return false
-    } else if (start < 0) {
-        start = start + len < 0 ? 0 : start + len;
-    }
-    if (Number.isNaN(param)) {
-        for (let i = start; i < len; i++) {
-            if (Number.isNaN(this[i])) return true;
-        }
-    } else {
-        for (let i = start; i < len; i++) {
-            if (this[i] === param) return true;
-        }
-    }
+  const len = this.length;
+  if (start >= this.length) {
     return false;
+  } else if (start < 0) {
+    start = start + len < 0 ? 0 : start + len;
+  }
+  if (Number.isNaN(param)) {
+    for (let i = start; i < len; i++) {
+      if (Number.isNaN(this[i])) return true;
+    }
+  } else {
+    for (let i = start; i < len; i++) {
+      if (this[i] === param) return true;
+    }
+  }
+  return false;
 };
