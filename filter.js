431d3c3f1e43e2409dc9be86d6aefa71605632f0
fuyuping <34242606+fuyuping@users.noreply.github.com>
2022-01-25 14:26:26 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,12 +1,13 @@
-Array.prototype.myFilter = function(cb, thisArg) {
-    const newArr = [], len = this.length
-    cb = cb.bind(thisArg)
-    if(len) {
-        for(let i = 0; i < len; i++) {
-            if(cb(this[i])) {
-                newArr.push(this[i])
-            }
-        }
+Array.prototype.myFilter = function (cb, thisArg) {
+  const newArr = [],
+    len = this.length;
+  cb = cb.bind(thisArg);
+  if (len) {
+    for (let i = 0; i < len; i++) {
+      if (cb(this[i])) {
+        newArr.push(this[i]);
+      }
     }
-    return newArr
-}
+  }
+  return newArr;
+};
