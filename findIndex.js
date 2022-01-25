d771817f8c74b49a1d43a010448e2e822095a5ba
fuyuping <34242606+fuyuping@users.noreply.github.com>
2022-01-25 14:13:33 +0800

    Create findIndex.js

---
+++ findIndex.js
@@ -0,0 +1,9 @@
+Array.prototype.myFindIndex = function(cb, thisArg) {
+    cb = cb.bind(thisArg)
+    for(let i = 0; i < this.length; i++) {
+        if(cb(this[i])) {
+            return i
+        }
+    }
+    return -1
+}
