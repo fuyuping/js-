02002022ec0b461ae2f5e3f33bee41d7beaf159b
fuyuping <34242606+fuyuping@users.noreply.github.com>
2022-01-25 14:17:31 +0800

    Create indexOf.js

---
+++ indexOf.js
@@ -0,0 +1,11 @@
+Array.prototype.myIndexOf = function (param, start = 0) {
+    const len = this.length
+    if (start >= len) return -1
+    if (start < 0) {
+        start = start + len < 0 ? 0 : start + len;
+    }
+    for (let i = start; i < len; i++) {
+        if (this[i] === param) return i;
+    }
+    return -1
+}
