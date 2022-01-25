22d95de0ee3c7bb9ec5ba4021bf28bb4ba043f60
fuyuping <34242606+fuyuping@users.noreply.github.com>
2022-01-25 14:08:10 +0800

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,6 +1,22 @@
+// call绑定this
 Array.prototype.myForEach = function(cb, thisArg) {
    const len = this.length
    for(let i = 0; i < len; i++) {
        cb.call(thisArg, this[i], i, this)
    }
+}
+// bind绑定this
+Array.prototype.myForEach = function (cb, thisArg) {
+      const len = this.length;
+      cb = cb.bind(thisArg)
+      for(let i = 0; i < len; i++) {
+        cb(this[i], i, this);
+      }
+}
+// apply绑定this
+Array.prototype.myForEach = function(cb, thisArg) {
+    const len = this.length
+    for(let i = 0; i < len; i++) {
+        cb.apply(thisArg, [this[i], i, this])
+    }
 }
